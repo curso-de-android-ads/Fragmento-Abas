280db6206ea8691e5d93e8f8a8c9e341b1fb16b2
WitaloCarlos <witalocarlos@gmail.com>
2015-05-21 14:12:09 -0300

    Exemplo de uso de fragmentos utilizando abas.

---
+++ app/src/main/java/com/cako/witalocarlos/fragmentoabas/FragmentoExemploB.java
@@ -0,0 +1,97 @@
+package com.cako.witalocarlos.fragmentoabas;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.zip.Inflater;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link FragmentoExemploB.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link FragmentoExemploB#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FragmentoExemploB extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FragmentoExemploB.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FragmentoExemploB newInstance(String param1, String param2) {
+        FragmentoExemploB fragment = new FragmentoExemploB();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public FragmentoExemploB() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_fragmento_exemplo_b, container, false);
+
+        Button bt = (Button) view.findViewById(R.id.bt_exemplo);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "Clicado", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+
+}
